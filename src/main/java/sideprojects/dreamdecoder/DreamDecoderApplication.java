364fd83c7faa63031f49cd276cd6debd98ad59a6
swlee <swo98@naver.com>
2025-08-29 21:41:37 +0900

    add: 꿈 저장을 이벤트 기반 비동기 처리하기 위해 eventlistener와 event객체 생성

--- src/main/java/sideprojects/dreamdecoder/DreamDecoderApplication.java
+++ src/main/java/sideprojects/dreamdecoder/DreamDecoderApplication.java
@@ -2,7 +2,9 @@ package sideprojects.dreamdecoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class DreamDecoderApplication {
 
--- src/main/java/sideprojects/dreamdecoder/infrastructure/external/openai/util/exception/OpenAiErrorCode.java
+++ src/main/java/sideprojects/dreamdecoder/infrastructure/external/openai/util/exception/OpenAiErrorCode.java
@@ -14,7 +14,11 @@ public enum OpenAiErrorCode implements ErrorCode {
     OPENAI_UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "OAI003", "OpenAI API 호출 중 예상치 못한 오류가 발생했습니다."),
     OPENAI_JSON_PARSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "OAI004", "OpenAI 응답 JSON 파싱에 실패했습니다."),
     OPENAI_SERVER_BUSY(HttpStatus.SERVICE_UNAVAILABLE, "OAI005", "현재 요청이 많아 처리가 지연되고 있습니다. 잠시 후 다시 시도해주세요."),
-    SEMAPHORE_ACQUIRE_FAIL(HttpStatus.SERVICE_UNAVAILABLE, "OAI006", "세마포어 획득 실패");
+    SEMAPHORE_ACQUIRE_FAIL(HttpStatus.SERVICE_UNAVAILABLE, "OAI006", "세마포어 획득 실패"),
+
+    // Distributed Lock
+    DUPLICATE_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "OAI007", "현재 요청을 처리 중입니다. 잠시 후 다시 시도해주세요."),
+    LOCK_ACQUIRE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "OAI008", "락 획득에 실패했습니다.");
 
     private final HttpStatus status;
     private final String code;
