7d80dfc33ed5592608ad587976f4ebc49eed433a
swlee <swo98@naver.com>
2025-09-10 21:42:51 +0900

    feat: DreamMapper에 dreamEmotion 및 tags 필드 매핑 추가

--- src/main/java/sideprojects/dreamdecoder/domain/dream/util/mapper/DreamMapper.java
+++ src/main/java/sideprojects/dreamdecoder/domain/dream/util/mapper/DreamMapper.java
@@ -4,14 +4,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import sideprojects.dreamdecoder.domain.dream.model.DreamModel;
 import sideprojects.dreamdecoder.domain.dream.persistence.DreamEntity;
+import sideprojects.dreamdecoder.domain.dream.util.enums.DreamEmotion;
 
 @Mapper(componentModel = "spring", uses = { DreamSymbolMapper.class })
 public interface DreamMapper {
 
     @Mapping(target = "dreamSymbols", source = "dreamTypes")
+    @Mapping(target = "dreamEmotion", source = "dreamEmotion")
+    @Mapping(target = "tags", source = "tags")
     DreamEntity toEntity(DreamModel dreamModel);
 
     @Mapping(target = "dreamTypes", source = "dreamSymbols")
+    @Mapping(target = "dreamEmotion", source = "dreamEmotion")
+    @Mapping(target = "tags", source = "tags")
     DreamModel toModel(DreamEntity dreamEntity);
 
 }
