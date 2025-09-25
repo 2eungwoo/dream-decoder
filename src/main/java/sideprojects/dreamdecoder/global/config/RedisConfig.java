feec2a76344108dc60a1db3dd13082397c1ba52d
swlee <swo98@naver.com>
2025-09-25 20:39:49 +0900

    bugfix:Spring의 기본 캐시 매니저를 Redisson의 캐시 매니저로 교체하여 라이브러리 충돌 해결

--- src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
+++ src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
@@ -1,19 +1,24 @@
 package sideprojects.dreamdecoder.global.config;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import sideprojects.dreamdecoder.domain.dream.util.enums.DreamEmotion;
 import sideprojects.dreamdecoder.domain.dream.util.enums.DreamType;
@@ -22,6 +27,19 @@ import sideprojects.dreamdecoder.infrastructure.external.openai.enums.AiStyle;
 @EnableCaching
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
+        return Redisson.create(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -33,14 +51,10 @@ public class RedisConfig {
     }
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory cf) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                new Jackson2JsonRedisSerializer<>(Object.class)))
-            .entryTtl(Duration.ofHours(24)); // Set TTL to 24 hours
-
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf)
-            .cacheDefaults(redisCacheConfiguration).build();
+    public CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<>();
+        config.put("dreamInterpretations", new CacheConfig(Duration.ofHours(24).toMillis(), Duration.ofHours(24).toMillis()));
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
 
     @Bean("dreamInterpretationKeyGenerator")
@@ -64,5 +78,4 @@ public class RedisConfig {
                 + sortedSymbolTypes;
         };
     }
-
 }
