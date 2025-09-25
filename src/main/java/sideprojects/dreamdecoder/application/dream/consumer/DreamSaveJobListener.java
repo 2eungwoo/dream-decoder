420ff8232de9f82a66534e36b247ad4b72aa1aa5
swlee <swo98@naver.com>
2025-09-25 20:34:13 +0900

    bugfix: 그룹 생성 시 스트림의 가장 처음부터 모든 메시지를 읽도록 시작 위치를 new StreamMessageId(0,0)으로 변경

--- src/main/java/sideprojects/dreamdecoder/application/dream/consumer/DreamSaveJobListener.java
+++ src/main/java/sideprojects/dreamdecoder/application/dream/consumer/DreamSaveJobListener.java
@@ -36,7 +36,7 @@ public class DreamSaveJobListener {
     private void setupStreamAndGroup() {
         RStream<String, String> stream = redissonClient.getStream(STREAM_KEY);
         try {
-            stream.createGroup(StreamCreateGroupArgs.name(CONSUMER_GROUP).id(StreamMessageId.ALL).makeStream());
+            stream.createGroup(StreamCreateGroupArgs.name(CONSUMER_GROUP).id(new StreamMessageId(0, 0)).makeStream());
             log.info("Redis Stream 컨슈머 그룹 생성 완료. 그룹: {}, 스트림 키: {}", CONSUMER_GROUP, STREAM_KEY);
         } catch (org.redisson.client.RedisException e) {
             if (e.getMessage().contains("BUSYGROUP")) {
