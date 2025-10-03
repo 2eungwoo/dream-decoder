d9dd0021a8bf42718110837e339e436593998d91
swlee <swo98@naver.com>
2025-10-03 18:42:52 +0900

    bugfix: Producer가 비어있는 interpretation 값을 메시지에 담으려다 NullPointerException으로 실패하던 문제를 해결. 초기 요청 데이터만 담아서 메시지를 정상적으로 발행

--- src/main/java/sideprojects/dreamdecoder/application/dream/producer/DreamSaveJobProducer.java
+++ src/main/java/sideprojects/dreamdecoder/application/dream/producer/DreamSaveJobProducer.java
@@ -7,12 +7,8 @@ import org.redisson.api.RStream;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.stream.StreamAddArgs;
 import org.springframework.stereotype.Component;
-import sideprojects.dreamdecoder.domain.dream.util.enums.DreamEmotion;
-import sideprojects.dreamdecoder.domain.dream.util.enums.DreamType;
-import sideprojects.dreamdecoder.infrastructure.external.openai.enums.AiStyle;
-import sideprojects.dreamdecoder.application.dream.producer.DreamSaveJobCommand;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -23,20 +19,18 @@ public class DreamSaveJobProducer {
     private static final String STREAM_KEY = "dream:save:jobs";
 
     private final RedissonClient redissonClient;
-    private final ObjectMapper objectMapper;
 
     public void publishJob(DreamSaveJobCommand command) {
         try {
             RStream<String, String> stream = redissonClient.getStream(STREAM_KEY);
-            Map<String, String> messageBody = Map.of(
-                    "userId", command.userId().toString(),
-                    "dreamContent", command.dreamContent(),
-                    "interpretationResult", command.interpretation(),
-                    "dreamEmotion", command.dreamEmotion().name(),
-                    "tags", command.tags(),
-                    "aiStyle", command.style().name(),
-                    "dreamTypes", objectMapper.writeValueAsString(command.types())
-            );
+
+            // interpretation, types는 비동기 처리 이후 생성 -> 초기 발행 시 미포함
+            Map<String, String> messageBody = new HashMap<>();
+            messageBody.put("userId", command.userId().toString());
+            messageBody.put("dreamContent", command.dreamContent());
+            messageBody.put("dreamEmotion", command.dreamEmotion().name());
+            messageBody.put("tags", command.tags());
+            messageBody.put("style", command.style().name());
 
             stream.add(StreamAddArgs.entries(messageBody));
             log.info("Redis Stream 메시지 발행 성공 (스트림 키: {})", STREAM_KEY);
