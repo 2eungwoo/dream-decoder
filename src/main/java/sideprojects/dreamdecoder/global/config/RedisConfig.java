fbcc93839cd8d98869624e4cdd9cfed6d13e9031
swlee <swo98@naver.com>
2025-10-24 04:56:17 +0900

    fix: api응답에서 날짜값이 배열로 나오는 문제를 해결하기 위해 기본ObjectMapper 날짜파싱 기능을 명시적으로 비활성화

--- src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
+++ src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import java.util.HashMap;
@@ -69,6 +70,7 @@ public class RedisConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 
