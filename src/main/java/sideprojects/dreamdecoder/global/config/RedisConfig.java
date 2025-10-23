7f7a6714f18384ea049d85e942b03e57a6938549
swlee <swo98@naver.com>
2025-10-24 04:42:42 +0900

    refactor: API용(@Primary), 캐시용(@Qualifier)으로 ObjectMapper를 분리, 캐시용 ObjectMapper를 PolymorphicTypeValidator와 JsonTypeInfo.As.PROPERTY를 명시적으로 적용 참고 출처: https://zzudev.tistory.com/51

--- src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
+++ src/main/java/sideprojects/dreamdecoder/global/config/RedisConfig.java
@@ -1,22 +1,26 @@
 package sideprojects.dreamdecoder.global.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -52,11 +56,31 @@ public class RedisConfig {
     }
 
     @Bean
-    public CacheManager cacheManager(RedissonClient redissonClient) {
+    public CacheManager cacheManager(RedissonClient redissonClient, @Qualifier("redisCacheObjectMapper") ObjectMapper objectMapper) {
         Map<String, CacheConfig> config = new HashMap<>();
         config.put("dreamInterpretations", new CacheConfig(Duration.ofHours(24).toMillis(), Duration.ofHours(24).toMillis()));
         config.put("dreamSymbols", new CacheConfig(Duration.ofHours(24).toMillis(), Duration.ofHours(24).toMillis()));
-        return new RedissonSpringCacheManager(redissonClient, config);
+
+        return new RedissonSpringCacheManager(redissonClient, config, new JsonJacksonCodec(objectMapper));
     }
 
+    @Primary
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean("redisCacheObjectMapper")
+    public ObjectMapper redisCacheObjectMapper() {
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+            .allowIfSubType(Object.class)
+            .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        return objectMapper;
+    }
 }
