90474e2b509e2fe9c68a9d4afa522eea5756d374
swlee <swo98@naver.com>
2025-10-26 17:29:03 +0900

    refactor: DreamSaveJobProducer에서  try-catch 제거하고 GlobalExceptionHandler에서 예외 전파되도록 수정

--- src/main/java/sideprojects/dreamdecoder/application/dream/producer/DreamSaveJobProducer.java
+++ src/main/java/sideprojects/dreamdecoder/application/dream/producer/DreamSaveJobProducer.java
@@ -21,22 +21,17 @@ public class DreamSaveJobProducer {
 
     public void publishJob(DreamSaveJobCommand command) {
         String streamKey = redisStreamProperties.getKey();
-        try {
-            RStream<String, String> stream = redissonClient.getStream(streamKey);
+        RStream<String, String> stream = redissonClient.getStream(streamKey);
 
-            // interpretation, types는 비동기 처리 이후 생성 -> 초기 발행 시 미포함
-            Map<String, String> messageBody = new HashMap<>();
-            messageBody.put("userId", command.userId().toString());
-            messageBody.put("dreamContent", command.dreamContent());
-            messageBody.put("dreamEmotion", command.dreamEmotion().name());
-            messageBody.put("tags", command.tags());
-            messageBody.put("style", command.style().name());
+        // interpretation, types는 비동기 처리 이후 생성 -> 초기 발행 시 미포함
+        Map<String, String> messageBody = new HashMap<>();
+        messageBody.put("userId", command.userId().toString());
+        messageBody.put("dreamContent", command.dreamContent());
+        messageBody.put("dreamEmotion", command.dreamEmotion().name());
+        messageBody.put("tags", command.tags());
+        messageBody.put("style", command.style().name());
 
-            stream.add(StreamAddArgs.entries(messageBody));
-            log.info("Redis Stream 메시지 발행 성공 (스트림 키: {})", streamKey);
-        } catch (Exception e) {
-            log.error("Redis Stream 메시지 발행 중 오류 발생 (유저 ID: {})", command.userId(), e);
-            // TODO: 메시지 발행 실패에 대한 강력한 예외 처리 또는 알림 로직 추가 예정
-        }
+        stream.add(StreamAddArgs.entries(messageBody));
+        log.info("Redis Stream 메시지 발행 성공 (스트림 키: {})", streamKey);
     }
 }
