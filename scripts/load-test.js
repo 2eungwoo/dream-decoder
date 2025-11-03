574379b6077e284f812bc89d0ddf84a5a312ec69
swlee <swo98@naver.com>
2025-11-03 17:30:30 +0900

    fix: k6요청에 auth-token 추가하고 유저리밋 일단 500

--- scripts/load-test.js
+++ scripts/load-test.js
@@ -3,16 +3,18 @@ import { check, sleep } from 'k6';
 import { randomUUID } from 'k6/crypto';
 
 export const options = {
-  vus: 10, // 10명의 가상 사용자가
-  duration: '30s', // 30초 동안 테스트를 수행합니다.
+  vus: 2, // 10명의 가상 사용자가
+  duration: '10s', // 30초 동안 테스트를 수행합니다.
 };
 
-const API_URL = 'http://localhost:8080/ai/chat?userId=1';
+const API_URL = 'http://localhost:8080/ai/chat?userId=3';
+const AUTH_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyNSIsImlhdCI6MTc2MjE1Nzk0OSwiZXhwIjoxNzYyMTYxNTQ5fQ.jztxcysHJAW0z-sDF6MM3cHxOcex3zyaRGe6ZpIw5h4';
 
 export default function () {
   const headers = {
     'Content-Type': 'application/json',
-    'Idempotency-Key': randomUUID(), // Idempotency Key 랜덤생성
+    'Authorization': AUTH_TOKEN,
+    'Idempotency-Key': `test-key-${__VU}-${__ITER}`, // Idempotency Key 랜덤생성
   };
 
   const payload = JSON.stringify({
